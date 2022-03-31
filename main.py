18b6b9ac8636d88ac8add31ab267f2b89504060f
Roberto Betancourt <rbetancourt@kavehome.com>
2022-03-31 10:50:20 +0200

    Improving bot At the branch Improving-bot  Changes to be confirmed: 	modified:     keep_alive.py 	modified:     main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ client = discord.Client()
 
 
 def get_random_deployer():
-    deployers = ["Jordi", "Jesús", "Huber", "Toni", "Mateo", "Didac"]
+    deployers = ['Jordi', 'Jesús', 'Huber', 'Toni', 'Mateo', 'Didac', 'Enrique']
     return random.choice(deployers)
 
 
 def get_random_phrase(deployer):
     phrases = [
         '{} calienta que deployeas!',
-        'Te ha tocado {}, mala suerte, ha deployear.',
-        'Adivinad a quien le toca fastidiar PRO, exacto a {}.'
+        'Te ha tocado {}, mala suerte, a deployear.',
+        'Adivinad a quién le toca fastidiar PRO, exacto a {}.'
     ]
     return random.choice(phrases).format(deployer)
 
@@ -33,35 +33,39 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+        if message.content == ('$deploy'):
+            await message.delete()
+            await message.channel.send(get_random_phrase(get_random_deployer()))
 
-    if message.content == ('$deploy'):
-        await message.delete()
-        await message.channel.send(get_random_phrase(get_random_deployer()))
+        elif message.content == ('$check-day'):
+            today_date = datetime.date.today().strftime('%A')
+            await message.delete()
+            await message.channel.send(today_date)
 
-    if message.content == ('$check-day'):
-        today_date = datetime.date.today().strftime('%A')
-        await message.delete()
-        await message.channel.send(today_date)
+        elif message.content == ('$ping'):
+            await message.delete()
 
-    if message.content == ('$ping'):
-        await message.delete()
+        elif message.content == ('$start_random_deploys'):
+            await message.delete()
+            await message.channel.send(
+                'Deploy Picker empezará a elegir a un deployer cada Martes y Jueves'
+                )
+            check_deploy_day.start()
 
-    if message.content == ('$start_random_deploys'):
-        await message.delete()
-        await message.channel.send("Deploy Picker empezará a elegir a un deployer cada Martes y Jueves")
-        check_deploy_day.start()
-
-    if message.content == ('$stop_random_deploys'):
-        await message.delete()
-        await message.channel.send("Deploy Picker parará de elegir a un deployer cada Martes y Jueves")
-        check_deploy_day.stop()
+        elif message.content == ('$stop_random_deploys'):
+            await message.delete()
+            await message.channel.send(
+                'Deploy Picker parará de elegir a un deployer cada Martes y Jueves'
+                )
+            check_deploy_day.stop()
 
 
 @tasks.loop(hours=24)
 async def check_deploy_day():
-    today_date = datetime.date.today().strftime('%A')
-    if (today_date == "Tuesday" or today_date == "Thursday"):
+    # Gets the weekday as a number where Tuesday it's 1 and Thursday is 3
+    # Comparing by integers improves the performance
+    today_date = datetime.date.weekday()
+    if today_date == 1 or today_date == 3:
         await client.get_channel(int(os.environ['CHANNEL_ID'])).send(
             get_random_phrase(get_random_deployer()))
 
