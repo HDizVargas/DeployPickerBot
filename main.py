41d71c0737050cea8382295d19fcc47a21879193
Roberto Betancourt <rbetancourt@kavehome.com>
2022-07-11 10:49:19 +0200

    Refactor bot

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import discord
+import json
 import os
 import random
-import datetime
-from discord.ext import tasks
-from keep_alive import keep_alive
+
+import discord
 from dotenv import load_dotenv
+
+from keep_alive import keep_alive
 
 load_dotenv()
 
 client = discord.Client()
+f = open('./deployers.json')
+deployers_json = json.load(f)
 
 
 def get_random_deployer():
-    deployers = ["Jordi B.", "Jesús", "Huber", "Toni", "Mateo", "Didac", "Enrique", "Jordi H."]
+    deployers = list(deployers_json.values())
     return random.choice(deployers)
 
 
@@ -20,7 +23,7 @@ def get_random_phrase(deployer):
     phrases = [
         '{} calienta que deployeas!',
         'Te ha tocado {}, mala suerte, a deployear.',
-        'Adivinad a quien le toca fastidiar PRO, exacto a {}.'
+        'Adivinad a quién le toca fastidiar PRO, exacto a {}.'
     ]
     return random.choice(phrases).format(deployer)
 
@@ -38,38 +41,8 @@ async def on_message(message):
         await message.delete()
         await message.channel.send(get_random_phrase(get_random_deployer()))
 
-    elif message.content == ('$check-day'):
-        today_date = datetime.date.today().strftime('%A')
-        await message.delete()
-        await message.channel.send(today_date)
-
     elif message.content == ('$ping'):
         await message.delete()
-
-    elif message.content == ('$start_random_deploys'):
-        await message.delete()
-        await message.channel.send(
-            "Deploy Picker empezará a elegir a un deployer cada Martes y Jueves"
-            )
-        check_deploy_day.start()
-
-    elif message.content == ('$stop_random_deploys'):
-        await message.delete()
-        await message.channel.send(
-            "Deploy Picker parará de elegir a un deployer cada Martes y Jueves"
-            )
-        check_deploy_day.stop()
-
-
-@tasks.loop(hours=24)
-async def check_deploy_day():
-    # Gets the weekday as a number where Tuesday it's 1 and Thursday is 3
-    # Comparing by integers improves the performance
-    today_date = datetime.date.weekday()
-    if today_date == 1 or today_date == 3:
-        await client.get_channel(int(os.environ['CHANNEL_ID'])).send(
-            get_random_phrase(get_random_deployer()))
-
 
 bot_token = os.environ['BOT_TOKEN']
 
